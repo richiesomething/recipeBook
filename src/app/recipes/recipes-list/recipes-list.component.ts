395156f15e59a71764d29716697609bdd3b4345f
richiesomething <rhdezc12@gmail.com>
2019-06-30 01:41:14 -0700

    recipe page with angular

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', '../~/Desktop/pic')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
